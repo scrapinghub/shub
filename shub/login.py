a0818ecd1ccb34f4c0c98f63cc816d55a6ebe494
Fabio J. Bertinatto <fabiojrb@gmail.com>
2014-12-03 15:05:50 -0200

    Add key validation
    Add a 'fatal' function to print a message to stderr and exit with error code 1

--- shub/login.py
+++ shub/login.py
@@ -1,16 +1,20 @@
-import sys, click
-from shub.utils import get_key_netrc, NETRC_FILE
+import re, click
+from shub.utils import fatal, get_key_netrc, NETRC_FILE
 
 @click.command(help='add Scrapinghug API key into the netrc file')
 def cli():
     if get_key_netrc():
-        sys.stderr.write('Key already exists in netrc file\n')
-        sys.exit(1)
+        fatal('Key already exists in netrc file')
     key = raw_input('Insert your Scrapinghub API key: ')
-    if key:
+    if key and is_valid_key(key):
         with open(NETRC_FILE, 'a+') as out:
             line = 'machine scrapinghub.com login {0} password ""'.format(key)
             out.write(line)
     else:
-        sys.stderr.write('Invalid key\n')
-        sys.exit(1)
+        fatal('Invalid key')
+
+def is_valid_key(key):
+    if re.findall(r'^[A-Fa-f\d]{32}$', key):
+        return True
+    else:
+        return False
--- shub/utils.py
+++ shub/utils.py
@@ -1,4 +1,4 @@
-import imp, os, netrc, ConfigParser
+import sys, imp, os, netrc
 
 SCRAPY_CFG_FILE = os.path.expanduser("~/.scrapy.cfg")
 NETRC_FILE = os.path.expanduser('~/.netrc')
@@ -32,3 +32,10 @@ def get_key_netrc():
         return
     if key:
         return key
+
+def fatal(message):
+    """Print a message to stderr and exit with error code 1"""
+    if not message.endswith('\n'):
+        message += '\n'
+    sys.stderr.write(message)
+    sys.exit(1)
