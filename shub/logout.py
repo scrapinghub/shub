fea3168c0312e90f96d8e1f6d0755a8ce57f0734
Fabio Bertinatto <fabiojrb@gmail.com>
2014-12-25 18:45:25 -0200

    Refactor out remove_sh_key() function
    Add test for remove_sh_key() function

--- shub/logout.py
+++ shub/logout.py
@@ -6,31 +6,25 @@ from shub.utils import get_key_netrc, NETRC_FILE
 def cli(context):
     if not get_key_netrc():
         context.fail('Key not found in netrc file')
-    error, msg = remove_sh_key()
-    if error:
-        context.fail(msg)
+    error_msg = remove_sh_key(NETRC_FILE)
+    if error_msg:
+        context.fail(error_msg)
     if get_key_netrc():
         context.fail(
             'Error removing key from the netrc file.'
             'Please, open the file ~/.netrc and remove it manually')
 
-def remove_sh_key():
-    key_re = r'machine\s+scrapinghub\.com\s+login\s+\w+\s+password\s+""\s*'
+def remove_sh_key(netrc_file):
     error_msg = ''
-    try:
-        with open(NETRC_FILE, 'r+') as out:
-            content = out.read()
-            content_new = re.sub(key_re, '', content)
-            if content_new == content:
-                error_msg = 'Regex didn\'t match. \
-                             Key wasn\'t removed from netrc file'
-            else:
-                out.seek(0)
-                out.truncate()
-                out.write(content_new)
-    except Exception as err:
-        error_msg = str(err)
-    if error_msg:
-        return True, error_msg
-    else:
-        return False, ''
+    key_re = r'machine\s+scrapinghub\.com\s+login\s+\w+\s+password\s+""\s*'
+    with open(netrc_file, 'r+') as out:
+        content = out.read()
+        content_new = re.sub(key_re, '', content)
+        if content_new == content:
+            error_msg = 'Regex didn\'t match. \
+                         Key wasn\'t removed from netrc file'
+        else:
+            out.seek(0)
+            out.truncate()
+            out.write(content_new)
+    return error_msg
