8977a409b7fb5ad66c081485155288ee57011c3d
aniversarioperu <aniversarioperu1@gmail.com>
2015-02-25 21:22:46 +0200

    Adding 'requests' as requirement in setup.py
    After ``pip install shub``, it fails to run due to the missing module
    ``requests`` as it is not installed because not being included in
    ``install_requires`` of ``setup.py``.
    
    However, ``requests`` does get installed when running ``python setup.py
    install``.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
-    install_requires=['click'],
+    install_requires=['click', 'requests'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
