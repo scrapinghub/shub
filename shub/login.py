ffb9868053e8a32ab04b190158d71ec74a57b82e
Jose Ricardo <zehzinho@gmail.com>
2015-06-19 16:45:12 -0400

    Deploy falling back to standard login credentials
    It was looking only for scrapy.cfg files. Now it also looks for the
    ~/.netrc file and $SHUB_APIKEY.

--- shub/login.py
+++ shub/login.py
@@ -1,5 +1,10 @@
-import os, re, click
+import os
+import re
+import click
 from shub.utils import get_key_netrc, NETRC_FILE
+
+SH_TOP_LEVEL_DOMAIN = 'scrapinghub.com'
+
 
 @click.command(help='add Scrapinghug API key into the netrc file')
 @click.pass_context
@@ -12,10 +17,12 @@ def cli(context):
             NETRC_FILE,
             os.O_CREAT | os.O_RDWR | os.O_APPEND, 0600)
         with os.fdopen(descriptor, 'a+') as out:
-            line = 'machine scrapinghub.com login {0} password ""\n'.format(key)
+            line = 'machine {0} login {1} password ""\n'.format(
+                SH_TOP_LEVEL_DOMAIN, key)
             out.write(line)
     else:
         context.fail('Invalid key')
+
 
 def is_valid_key(key):
     return bool(re.match(r'[A-Fa-f\d]{32}$', key))
