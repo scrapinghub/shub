8d000f8276d97e026c40931b7f6fd319a034b194
Jose Ricardo <zehzinho@gmail.com>
2015-06-25 17:19:58 -0400

    Display log message at deploy-reqs start
    Just to give the user some feedback, since the download can take some
    time.

--- shub/deploy_reqs.py
+++ shub/deploy_reqs.py
@@ -5,6 +5,7 @@ import tempfile
 import shutil
 
 from shub.utils import run, decompress_egg_files
+from shub.click_utils import log
 from shub import utils
 
 
@@ -35,10 +36,11 @@ def _mk_and_cd_eggs_tmpdir():
 def _download_egg_files(eggs_dir, requirements_file):
     editable_src_dir = tempfile.mkdtemp(prefix='pipsrc')
 
+    log('Downloading eggs...')
     try:
         pip_cmd = ("pip install -d {eggs_dir} -r {requirements_file}"
                    " --src {editable_src_dir} --no-deps --no-use-wheel")
-        print(run(pip_cmd.format(eggs_dir=eggs_dir,
+        log(run(pip_cmd.format(eggs_dir=eggs_dir,
                                  editable_src_dir=editable_src_dir,
                                  requirements_file=requirements_file)))
     finally:
