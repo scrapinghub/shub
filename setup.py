2e7b743fa5d452e43d01a4d6a445092164af826c
Jose Ricardo <zehzinho@gmail.com>
2015-06-29 11:39:48 -0400

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='shub',
-    version='1.3.0',
+    version='1.3.1',
     packages=['shub'],
     url='https://doc.scrapinghub.com/shub.html',
     description='Scrapinghub Command Line Client',
