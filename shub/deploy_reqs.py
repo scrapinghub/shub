96ace561973c4996c7753f1c87676e1a1dddc4f2
José Ricardo <root@josericardo.eti.br>
2015-09-03 09:09:02 -0400

    Revert "Use the project id in the scrapy.cfg as default for deploy-reqs"

--- shub/deploy_reqs.py
+++ shub/deploy_reqs.py
@@ -7,25 +7,19 @@ import shutil
 from shub.utils import run, decompress_egg_files
 from shub.click_utils import log
 from shub import utils
-from shub import scrapycfg
 from shub.auth import find_api_key
 
 
 @click.command(help="Build and deploy eggs from requirements.txt")
-@click.option("-p", "--project", help="the project ID to deploy to",
-              type=click.INT)
+@click.argument("project_id", required=True)
 @click.argument("requirements_file", required=True)
-def cli(project, requirements_file):
+def cli(project_id, requirements_file):
     """Just a wrapper around the deploy_egg module"""
-    main(project, requirements_file)
+    main(project_id, requirements_file)
 
 
 def main(project_id, requirements_file):
-    target = scrapycfg.get_target('default')
-    project_id = scrapycfg.get_project(target, project_id)
     apikey = find_api_key()
-    log('Deploying requirements to project "%s"' % project_id)
-
     requirements_full_path = os.path.abspath(requirements_file)
     eggs_tmp_dir = _mk_and_cd_eggs_tmpdir()
     _download_egg_files(eggs_tmp_dir, requirements_full_path)
@@ -49,7 +43,7 @@ def _download_egg_files(eggs_dir, requirements_file):
         pip_cmd = ("pip install -d {eggs_dir} -r {requirements_file}"
                    " --src {editable_src_dir} --no-deps --no-use-wheel")
         log(run(pip_cmd.format(eggs_dir=eggs_dir,
-                               editable_src_dir=editable_src_dir,
-                               requirements_file=requirements_file)))
+                                 editable_src_dir=editable_src_dir,
+                                 requirements_file=requirements_file)))
     finally:
         shutil.rmtree(editable_src_dir, ignore_errors=True)
