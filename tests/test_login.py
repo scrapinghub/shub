a96727422390daf063654a55d262ce4e4766cc2c
Jose Ricardo <zehzinho@gmail.com>
2015-11-04 12:57:30 -0300

    Refactor the login tests
    I felt the tests intents were lost among the mock machinery.

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,12 +5,10 @@ from mock import Mock, patch
 from click.testing import CliRunner
 from shub import login
 
+VALID_KEY = 32 * '1'
 
 class LoginTest(unittest.TestCase):
-    VALID_KEY = 32 * '1'
-
     def setUp(self):
-        login.auth.NETRC_FILE = ''
         self.runner = CliRunner()
 
     def test_login_writes_input_key_to_netrc_file(self):
@@ -19,63 +17,63 @@ class LoginTest(unittest.TestCase):
         login.auth.write_key_netrc = fake_netrc_writer
 
         # when
-        with patch.object(login, '_is_valid_apikey', return_value=True):
-            self.runner.invoke(login.cli, input=self.VALID_KEY)
+        self._run()
 
         # then
-        fake_netrc_writer.assert_called_with(self.VALID_KEY)
+        fake_netrc_writer.assert_called_with(VALID_KEY)
 
     def test_login_suggests_scrapy_cfg_username_as_key(self):
         scrapy_cfg_with_username = """
 [deploy]
 username = KEY_SUGGESTION
         """
-
-        with self.runner.isolated_filesystem():
-            with open('scrapy.cfg', 'w') as f:
-                f.write(scrapy_cfg_with_username)
-
-            result = self.runner.invoke(login.cli, input='123')
-            err = 'Unexpected output: %s' % result.output
-            self.assertTrue('KEY_SUGGESTION' in result.output, err)
+        result = self._run(files={'scrapy.cfg': scrapy_cfg_with_username}, read_scrapy_cfg=True)
+        err = 'Unexpected output: %s' % result.output
+        self.assertTrue('KEY_SUGGESTION' in result.output, err)
 
     def test_login_suggests_shub_apikey_as_key(self):
-        with self.runner.isolated_filesystem():
-            with patch.object(login, '_read_scrapy_cfg_key', return_value=None):
-                result = self.runner.invoke(login.cli, input='123',
-                                            env={'SHUB_APIKEY': 'SHUB_APIKEY_VALUE'})
-
-                err = 'Unexpected output: %s' % result.output
-                self.assertTrue('SHUB_APIKEY_VALUE' in result.output, err)
+        result = self._run(env={'SHUB_APIKEY': 'SHUB_APIKEY_VALUE'})
+        err = 'Unexpected output: %s' % result.output
+        self.assertTrue('SHUB_APIKEY_VALUE' in result.output, err)
 
     def test_login_can_handle_invalid_scrapy_cfg(self):
-        # given
-        invalid_scrapy_cfg = 'invalid content'
-
-        with self.runner.isolated_filesystem() as fs:
-            login.auth.NETRC_FILE = os.path.join(fs, '.netrc')
-
-            with open('scrapy.cfg', 'w') as f:
-                f.write(invalid_scrapy_cfg)
-
-            with patch.object(login, '_is_valid_apikey', return_value=True):
-                # when
-                result = self.runner.invoke(login.cli, input=self.VALID_KEY)
-
-                # then
-                self.assertEqual(0, result.exit_code, result.exception)
+        result = self._run(files={'scrapy.cfg': 'invalid content'})
+        self.assertEqual(0, result.exit_code, result.exception)
 
     def test_login_attempt_after_login_doesnt_lead_to_an_error(self):
         with self.runner.isolated_filesystem() as fs:
-            login.auth.NETRC_FILE = os.path.join(fs, '.netrc')
-
-            with patch.object(login, '_is_valid_apikey', return_value=True):
-                # given
-                self.runner.invoke(login.cli, input=self.VALID_KEY)
-
-                # when
-                result = self.runner.invoke(login.cli, input=self.VALID_KEY)
+            # when
+            self._run(fs=fs)
+            result = self._run(fs=fs)
 
             # then
             self.assertEqual(0, result.exit_code)
             self.assertTrue('already logged in' in result.output, result.output)
+
+    def _run(self, user_input=VALID_KEY, files=None, fs=None, env=None, read_scrapy_cfg=False):
+        """Invokes the login cli on an isolated filesystem"""
+
+        def write_local_test_files():
+            for path, content in (files or {}).iteritems():
+                with open(path, 'w') as f:
+                    f.write(content)
+
+        def invoke():
+            return self.runner.invoke(login.cli, input=user_input, env=env)
+
+        def run():
+            write_local_test_files()
+            login.auth.NETRC_FILE = os.path.join(fs, '.netrc')
+
+            with patch.object(login, '_is_valid_apikey', return_value=True):
+                if read_scrapy_cfg:
+                    return invoke()
+
+                with patch.object(login, '_read_scrapy_cfg_key', return_value=None):
+                    return invoke()
+
+        if fs:
+            return run()
+
+        with self.runner.isolated_filesystem() as fs:
+            return run()
