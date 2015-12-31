dd9687fe0254f28c9d144b1d91794d4c024a3cfb
Jakob de Maeyer <jakobdm1@gmail.com>
2015-12-31 14:42:49 +0100

    Use more meaningful exit codes

--- shub/exceptions.py
+++ shub/exceptions.py
@@ -1,3 +1,10 @@
+"""
+shub-specific exceptions.
+
+Exit codes follow the sysexits.h convention:
+https://www.freebsd.org/cgi/man.cgi?query=sysexits&sektion=3
+"""
+
 from click import BadParameter, ClickException
 
 
@@ -7,10 +14,14 @@ class ShubException(ClickException):
 
 
 class MissingAuthException(ShubException):
+    # EX_NOPERM would be more appropriate here but would forbid distinguishing
+    # this from InvalidAuth by exit code
+    exit_code = 67  # EX_NOUSER
     default_msg = "Not logged in. Please run 'shub login' first."
 
 
 class InvalidAuthException(ShubException):
+    exit_code = 77  # EX_NOPERM
     default_msg = ("Authentication failure. Please make sure that your API key"
                    " is valid.")
 
@@ -22,24 +33,27 @@ class AlreadyLoggedInException(ShubException):
 
 
 class ConfigParseException(ShubException):
+    exit_code = 65  # EX_DATAERR
     default_msg = "Unable to parse configuration."
 
 
 class BadConfigException(ShubException):
+    exit_code = 78  # EX_CONFIG
     # Should be initialised with more specific message
     default_msg = "Please check your scrapinghub.yml."
 
 
 class NotFoundException(ShubException):
     # Should be initialised with more specific message
+    exit_code = 69  # EX_UNAVAILABLE
     default_msg = "Not found."
 
 
 class BadParameterException(BadParameter):
-    # Proxy for clearer imports
-    pass
+    exit_code = 64  # EX_USAGE
 
 
 class RemoteErrorException(ShubException):
+    exit_code = 76  # EX_PROTOCOL
     # Should be initialised with more specific message
     default_msg = "Remote error."
