89ced56a72bb5a168c862351344baa37c5a64603
Viktor Shlapakov <vshlapakov@gmail.com>
2016-09-07 22:32:16 +0300

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shub-image',
-    version='0.2.3',
+    version='0.2.4',
     packages=['shub_image'],
     description='Scrapinghub release tool',
     long_description=open('README.rst').read(),
@@ -20,7 +20,7 @@ setup(
         'click',
         'requests',
         'six',
-        'shub>=0.2.3',
+        'shub>=0.2.4',
         'docker-py',
     ],
     classifiers=[
