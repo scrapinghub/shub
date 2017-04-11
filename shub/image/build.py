90d9b44005ec76d03f794831e21754a058d97db9
Viktor Shlapakov <vshlapakov@gmail.com>
2017-04-11 11:00:26 +0300

    Refactor messy logic into classes

--- shub/image/build.py
+++ shub/image/build.py
@@ -26,6 +26,7 @@ shub utils itself).
 """
 
 BUILD_STEP_REGEX = re.compile(r'Step (\d+)/(\d+) :*')
+BUILT_REGEX = re.compile(r'Successfully built ([0-9a-f]+)')
 
 
 @click.command(help=HELP, short_help=SHORT_HELP)
@@ -52,42 +53,78 @@ def build_cmd(target, version, skip_tests):
     if not os.path.exists(os.path.join(project_dir, 'Dockerfile')):
         raise shub_exceptions.BadParameterException(
             "Dockerfile is not found, please use shub image 'init' command")
-    bar, is_built = None, False
-    verbose = utils.is_verbose()
-    for data in client.build(path=project_dir, tag=image_name, decode=True):
-        if 'stream' in data:
-            if not verbose:
-                bar = _create_or_update_progress_bar(bar, data, verbose)
-            utils.debug_log("{}".format(data['stream'][:-1]))
-            is_built = re.search(
-                r'Successfully built ([0-9a-f]+)', data['stream'])
-        elif 'error' in data:
-            tqdm.write("Error {}:\n{}".format(
-                data['error'], data['errorDetail']))
-    if bar:
-        bar.close()
-    if not is_built:
-        raise shub_exceptions.RemoteErrorException(
-            "Build image operation failed")
+    if utils.is_verbose():
+        build_progress_cls = _LoggedBuildProgress
+    else:
+        build_progress_cls = _BuildProgress
+    events = client.build(path=project_dir, tag=image_name, decode=True)
+    build_progress = build_progress_cls(events)
+    build_progress.show()
     click.echo("The image {} build is completed.".format(image_name))
     # Test the image content after building it
     if not skip_tests:
         test_cmd(target, version)
 
 
-def _create_or_update_progress_bar(bar, event, verbose):
-    """Helper to handle build steps and track progress."""
-    step_row = BUILD_STEP_REGEX.match(event['stream'])
-    if step_row:
+class _LoggedBuildProgress(object):
+    """Visualize build progress in verbose mode.
+
+    Output all the events received from the docker daemon.
+    """
+    def __init__(self, events):
+        self.events = events
+
+    def show(self):
+        for event in self.events:
+            self.handle_event(event)
+
+    def handle_event(self, event):
+        if 'stream' in event:
+            self.handle_stream_event(event)
+        if 'error' in event:
+            tqdm.write("Error {}: {}".format(event['error'],
+                                             event['errorDetail']))
+            raise shub_exceptions.RemoteErrorException(
+                "Build image operation failed")
+
+    def handle_stream_event(self, event):
+        utils.debug_log("{}".format(event['stream'][:-1]))
+
+
+class _BuildProgress(_LoggedBuildProgress):
+    """Visualize build progress in non-verbose mode.
+
+    Show total progress bar.
+    """
+
+    def __init__(self, events):
+        super(_BuildProgress, self).__init__(events)
+        self.bar = None
+        self.is_built = False
+
+    def show(self):
+        super(_BuildProgress, self).show()
+        if self.bar:
+            self.bar.close()
+        if not self.is_built:
+            raise shub_exceptions.RemoteErrorException(
+                "Build image operation failed")
+
+    def handle_stream_event(self, event):
+        if BUILT_REGEX.search(event['stream']):
+            self.is_built = True
+            return
+        step_row = BUILD_STEP_REGEX.match(event['stream'])
+        if not step_row:
+            return
         step_id, total = [int(val) for val in step_row.groups()]
-        if not bar:
-            bar = utils.create_progress_bar(
+        if not self.bar:
+            self.bar = utils.create_progress_bar(
                 total=total,
                 desc='layers',
                 # don't need rate here, let's simplify the bar
                 bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}',
             )
         # ignore onbuild sub-steps
-        if step_id > bar.pos and bar.total == total:
-            bar.update()
-    return bar
+        if step_id > self.bar.pos and self.bar.total == total:
+            self.bar.update()
