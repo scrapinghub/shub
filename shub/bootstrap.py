37750aa6a841008ba1dcb05bc3e12e8b54db4d89
Jakob de Maeyer <jakobdm1@gmail.com>
2017-05-24 13:56:17 +0200

    Add project cloning to shub bootstrap command

--- shub/bootstrap.py
+++ shub/bootstrap.py
@@ -1,11 +1,18 @@
 from __future__ import absolute_import
 
+import os
+import shutil
+import tempfile
+import zipfile
+
 import click
 import requests
 import yaml
 from click.formatting import HelpFormatter
+from six import BytesIO
 
-from shub.exceptions import RemoteErrorException
+from shub.exceptions import (
+    BadParameterException, NotFoundException, RemoteErrorException)
 
 
 EXAMPLE_REPO = "scrapinghub/custom-images-examples"
@@ -50,8 +57,24 @@ def list_projects_callback(ctx, param, value):
 @click.option('-l', '--list', 'list_projects', help='list available projects',
               is_flag=True, callback=list_projects_callback,
               expose_value=False, is_eager=True)
-def cli():
-    pass
+@click.argument('project')
+@click.argument('target_dir', required=False)
+def cli(project, target_dir):
+    target_dir = os.path.normpath(
+        os.path.join(os.getcwd(), target_dir or project))
+    if os.path.exists(target_dir):
+        raise BadParameterException(
+            "Target directory %s already exists, please delete it or supply a "
+            "non-existing target." % target_dir)
+    projects = get_available_projects()
+    if project not in projects:
+        raise NotFoundException(
+            "There is no example project named '%s'. Run 'shub bootstrap -l' "
+            "to get a list of all available projects." % project)
+    click.echo("Downloading custom image examples")
+    repo_zip = get_repo_zip(EXAMPLE_REPO)
+    click.echo("Cloning project '%s' into %s" % (project, target_dir))
+    unzip_project(repo_zip, project=projects[project], target_dir=target_dir)
 
 
 def get_available_projects():
@@ -76,3 +99,26 @@ def list_projects(projects):
                  for name, info in projects.items()],
                 key=lambda x: x[0]))
     click.echo(formatter.getvalue().strip())
+
+
+def get_repo_zip(repo):
+    zip_url = "https://github.com/%s/archive/master.zip" % repo
+    resp = requests.get(zip_url)
+    return zipfile.ZipFile(BytesIO(resp.content))
+
+
+def unzip_project(repo_zip, project, target_dir):
+    filenames = repo_zip.namelist()
+    repo_dirname = filenames[0]
+    project_filenames = [
+        fn
+        for fn in filenames
+        if fn.startswith(repo_dirname + project['path'])
+    ]
+    tempdir = tempfile.mkdtemp()
+    repo_zip.extractall(path=tempdir, members=project_filenames)
+    shutil.move(
+        os.path.join(tempdir, repo_dirname, project['path']),
+        target_dir,
+    )
+    shutil.rmtree(tempdir)
