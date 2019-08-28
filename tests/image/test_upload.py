98995a099a767218e3867dbd774646bcae500770
Viktor Shlapakov <vshlapakov@gmail.com>
2019-08-28 14:23:23 +0300

    Fix the broken test

--- tests/image/test_upload.py
+++ tests/image/test_upload.py
@@ -15,10 +15,11 @@ class TestUploadCli(TestCase):
             cli, ["dev", "-v", "--version", "test",
                   "--username", "user", "--password", "pass",
                   "--email", "mail", "--async", "--apikey", "apikey",
-                  "--skip-tests", "--no-cache", "-f", "Dockerfile"])
+                  "--skip-tests", "--no-cache", "-f", "Dockerfile", "--reauth"])
         assert result.exit_code == 0
         build.assert_called_with('dev', 'test', True, True, filename='Dockerfile')
         push.assert_called_with(
-            'dev', 'test', 'user', 'pass', 'mail', "apikey", False, skip_tests=True)
+            'dev', 'test', 'user', 'pass', 'mail', "apikey", False, reauth=True,
+            skip_tests=True)
         deploy.assert_called_with(
             'dev', 'test', 'user', 'pass', 'mail', "apikey", False, True)
