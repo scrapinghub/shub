31edbc1f87c4598350eb7a38023841c2bf78d0e6
Rafael Capucho <rafael.capucho@gmail.com>
2021-01-07 19:45:25 -0300

    let it fail first

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'Environment :: Console',
         'Topic :: Internet :: WWW/HTTP',
